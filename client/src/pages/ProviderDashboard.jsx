5fea94b030dbe090a7f463e2d0474034d76259be
walid-wnany <74496207+walid-wnany@users.noreply.github.com>
2026-02-25 16:11:49 -0800

    Create ProviderDashboard component

---
+++ client/src/pages/ProviderDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProviderDashboard = () => {
+    return (
+        <div>
+            <h1>Provider Dashboard</h1>
+            {/* Add your component logic here */}
+        </div>
+    );
+};
+
+export default ProviderDashboard;
