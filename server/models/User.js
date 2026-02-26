f4b70c4eb147c0c3c6e479e27bbb901067926358
walid-wnany <74496207+walid-wnany@users.noreply.github.com>
2026-02-25 16:05:57 -0800

    Create User model schema

---
+++ server/models/User.js
@@ -0,0 +1,19 @@
+'use strict';
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+userSchema.pre('save', function (next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
