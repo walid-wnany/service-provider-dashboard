a4ce46bf3adff7b58f53d676653e17a6d690ed2b
walid-wnany <74496207+walid-wnany@users.noreply.github.com>
2026-02-25 16:08:08 -0800

    Create Booking mongoose schema

---
+++ server/models/Booking.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    service: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Service',
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'confirmed', 'canceled'],
+        default: 'pending'
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Booking', bookingSchema);
