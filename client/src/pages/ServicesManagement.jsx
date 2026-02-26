6d74fa1c8f35d7c6039175b2276b3d7a9799e9fd
walid-wnany <74496207+walid-wnany@users.noreply.github.com>
2026-02-25 16:12:07 -0800

    Create ServicesManagement component

---
+++ client/src/pages/ServicesManagement.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServicesManagement = () => {
+    return (
+        <div>
+            <h1>Services Management</h1>
+            {/* Add your service management features here */}
+        </div>
+    );
+};
+
+export default ServicesManagement;
