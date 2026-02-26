5263b34ad82d83d465c8e38ef1fc90a2f2059d8e
walid-wnany <74496207+walid-wnany@users.noreply.github.com>
2026-02-25 16:12:30 -0800

    Create AvailabilityCalendar component

---
+++ client/src/pages/AvailabilityCalendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AvailabilityCalendar = () => {
+    return (
+        <div>
+            <h1>Availability Calendar</h1>
+            {/* Calendar component implementation goes here */}
+        </div>
+    );
+};
+
+export default AvailabilityCalendar;
